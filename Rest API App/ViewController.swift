97a132aedcfe27e3203a78ee8bd62ac5972224d0
Anton Pavlov <toni.pavlov.85@bk.ru>
2023-02-01 12:40:21 +0300

    add SnapKit and new code for TableView

--- Rest API App/ViewController.swift
+++ Rest API App/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        tableView.dataSource = self
+        tableView.register(tableViewCell.self, forCellReuseIdentifier: "cell")
     }
 
-
+// MARK: - Private properties
+    
+    private var tableView = UITableView()
 }
 
+// MARK: - TableViewDataSource
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        
+        return cell
+    }
+    
+    
+}
