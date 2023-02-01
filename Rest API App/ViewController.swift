dba09dd2e68104950bb6a0761f8109280707c916
Anton Pavlov <toni.pavlov.85@bk.ru>
2023-02-01 15:47:01 +0300

    Remove all the code

--- Rest API App/ViewController.swift
+++ Rest API App/ViewController.swift
@@ -6,38 +6,15 @@
 //
 
 import UIKit
-import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(tableView)
-        tableView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+    
         }
-        tableView.dataSource = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        
-        APIManager.shared.getData()
+       
     }
 
-// MARK: - Private properties
-    
-    private var tableView = UITableView()
-}
 
-// MARK: - TableViewDataSource
 
-extension ViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
-        cell.textLabel?.text = "Hello"
-        return cell
-    }
-    
-    
-}
