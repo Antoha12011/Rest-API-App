9b54228f3ad003c512fab351328fd3c71dd9423f
Anton Pavlov <toni.pavlov.85@bk.ru>
2023-03-11 16:16:59 +0300

    Add new VC, add new file

---
+++ Rest API App/HeroViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HeroViewController.swift
+//  Rest API App
+//
+//  Created by Антон Павлов on 11.03.2023.
+//
+
+import UIKit
+
+class HeroViewController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+}
